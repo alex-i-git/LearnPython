cd65ce0d0ed763aa7ea701c4f747398ff68b37af
Alex Izmaylov <a.izmaylove@gmail.com>
2016-10-01 12:19:50 +0300

    Бот telegram

--- bot.py
+++ bot.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
 
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+def talk_to_me(bot, update):
+    print('Пришло сообщение: %s' % update.message.text)
+    bot.sendMessage(update.message.chat_id, update.message.text)
+    
+def main():
+    
+    updater = Updater("API_KEY")
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler([Filters.text], talk_to_me))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
