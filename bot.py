11618ee7c2f4bc5c3bb380a0be1c5904574a352f
Alex Izmaylov <a.izmaylove@gmail.com>
2016-10-05 17:53:09 +0300

    Бот заработал

--- bot.py
+++ bot.py
@@ -2,20 +2,29 @@
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+#Updater - связь с telegram
+#CommandHandler - обработчик команд
+#MessageHandler - обработчик сообщений
+#Filters - фильтрует сообщения, кроме текстовых
+
+def start(bot, update):	# Пишет в консоль о вызове старт в клиенте telegram
+	print("Вызван /start")
+	bot.sendMessage(update.message.chat_id, text="Привет, человек! Я бот, который помогает")
+
 def talk_to_me(bot, update):
     print('Пришло сообщение: %s' % update.message.text)
     bot.sendMessage(update.message.chat_id, update.message.text)
     
-def main():
+def run_bot():
     
-    updater = Updater("API_KEY")
+    updater = Updater("195034229:AAG8LDc4Q-O0NL991wza6ovbwQKVZ1zT2Rk")
 
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(MessageHandler([Filters.text], talk_to_me))
 
-    updater.start_polling()
+    updater.start_polling() # опрашивает telegram на наличие сообщений
     updater.idle()
 
 if __name__ == '__main__':
-    main()
+    run_bot()
